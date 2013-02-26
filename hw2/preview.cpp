0e299f6d61a0d7a7e6aeb7303076742e85517974
Michael Driscoll <mbdriscoll@gmail.com>
2013-02-25 18:46:48 -0800

    reorg

--- hw2/preview.cpp
+++ hw2/preview.cpp
@@ -62,8 +62,8 @@ Light::Render() {
 
 void
 Light::Init() {
-    /*
     GLenum n = GL_LIGHT0 + lnum;
+    /*
     glEnable(n);
 
     glLoadMatrixf( (const float *) &xform[0] );
@@ -78,6 +78,26 @@ Light::Init() {
     glLightf(n, GL_LINEAR_ATTENUATION,    material.atten[1]);
     glLightf(n, GL_QUADRATIC_ATTENUATION, material.atten[2]);
     */
+
+    const GLfloat light_ambient[]  = { 0.0f, 0.0f, 0.0f, 1.0f };
+    const GLfloat light_diffuse[]  = { 1.0f, 1.0f, 1.0f, 1.0f };
+    const GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+    const GLfloat light_position[] = { 2.0f, 5.0f, 5.0f, 0.0f }; 
+    const GLfloat mat_ambient[]    = { 0.7f, 0.7f, 0.7f, 1.0f };
+    const GLfloat mat_diffuse[]    = { 0.8f, 0.8f, 0.8f, 1.0f };
+    const GLfloat mat_specular[]   = { 1.0f, 1.0f, 1.0f, 1.0f };
+    const GLfloat high_shininess[] = { 100.0f };
+
+    glEnable(n);
+    glLightfv(n, GL_AMBIENT,  light_ambient);
+    glLightfv(n, GL_DIFFUSE,  light_diffuse);
+    glLightfv(n, GL_SPECULAR, light_specular);
+    glLightfv(n, GL_POSITION, light_position);
+
+    glMaterialfv(GL_FRONT, GL_AMBIENT,   mat_ambient);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE,   mat_diffuse);
+    glMaterialfv(GL_FRONT, GL_SPECULAR,  mat_specular);
+    glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
 }
 
 void
@@ -90,15 +110,6 @@ Scene::Render() {
 }
 
 void initGL(void) {
-    const GLfloat light_ambient[]  = { 0.0f, 0.0f, 0.0f, 1.0f };
-    const GLfloat light_diffuse[]  = { 1.0f, 1.0f, 1.0f, 1.0f };
-    const GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
-    const GLfloat light_position[] = { 2.0f, 5.0f, 5.0f, 0.0f }; 
-    const GLfloat mat_ambient[]    = { 0.7f, 0.7f, 0.7f, 1.0f };
-    const GLfloat mat_diffuse[]    = { 0.8f, 0.8f, 0.8f, 1.0f };
-    const GLfloat mat_specular[]   = { 1.0f, 1.0f, 1.0f, 1.0f };
-    const GLfloat high_shininess[] = { 100.0f };
-
     glClearColor(0,0,0,0); // bg color
     glEnable(GL_CULL_FACE);
     glCullFace(GL_BACK);
@@ -106,22 +117,10 @@ void initGL(void) {
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LESS);
 
-    glEnable(GL_LIGHT0);
     glEnable(GL_NORMALIZE);
     glEnable(GL_COLOR_MATERIAL);
     glEnable(GL_LIGHTING);
     glShadeModel(GL_FLAT);
-
-    glLightfv(GL_LIGHT0, GL_AMBIENT,  light_ambient);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE,  light_diffuse);
-    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
-    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-
-    glMaterialfv(GL_FRONT, GL_AMBIENT,   mat_ambient);
-    glMaterialfv(GL_FRONT, GL_DIFFUSE,   mat_diffuse);
-    glMaterialfv(GL_FRONT, GL_SPECULAR,  mat_specular);
-    glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
-
 }
 
 void display(void) {
